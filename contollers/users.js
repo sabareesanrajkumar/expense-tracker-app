862b46d81ae8dc85004e8f2cc72228430b453061
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2024-12-05 03:34:31 +0530

    login page frontend

--- contollers/users.js
+++ contollers/users.js
@@ -5,7 +5,6 @@ exports.createUser = async (req, res, next) => {
     const newUser = await Users.create(req.body);
     return res.status(200).json({ success: true, message: "user created" });
   } catch (err) {
-    console.log(err);
     if (err.name === "SequelizeUniqueConstraintError") {
       return res
         .status(500)
@@ -14,5 +13,34 @@ exports.createUser = async (req, res, next) => {
     return res
       .status(500)
       .json({ success: false, message: "failed to create user" });
+  }
+};
+
+exports.logIn = async (req, res, next) => {
+  try {
+    const searchUser = await Users.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!searchUser) {
+      return res
+        .status(500)
+        .json({ success: false, message: "User not found. Please Sign Up" });
+    }
+
+    if (searchUser.passWord !== req.body.passWord) {
+      return res
+        .status(500)
+        .json({ success: true, message: "password did not match" });
+    }
+
+    return res.status(200).json({ success: true, message: "login successful" });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ success: false, message: "failed to login user" });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require("../contollers/users");
 
 router.post("/signup", userController.createUser);
+router.post("/login", userController.logIn);
 
 module.exports = router;
